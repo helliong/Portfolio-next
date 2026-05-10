6ce225ca045edec026bfbdfc38930dff48b168a4
helliong <90344433+helliong@users.noreply.github.com>
2026-05-10 15:46:43 +0500

    Updates

--- _next/static/c7yrLkDyUZSKMJ9a1sffj/_buildManifest.js
+++ _next/static/c7yrLkDyUZSKMJ9a1sffj/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/portfolio-next//_next/:path+",
+        "destination": "/portfolio-next/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
